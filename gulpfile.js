779e292c40b0f0e9a19b6edffb42ceb3e8a25a76
孙颖洲 <sunyz94123@qq.com>
2025-07-08 17:17:44 +0800

    chore: bs refresh

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 import gulp from 'gulp'
 import less from 'gulp-less'
 import postcss from 'gulp-postcss'
+import browserSync from 'browser-sync'
+
+const bs = browserSync.create()
 
 const handleStyle = () => {
-  return gulp.src('src/style/style.less').pipe(less()).pipe(postcss()).pipe(gulp.dest('src/css'))
+  return gulp.src('src/style/style.less').pipe(less()).pipe(postcss()).pipe(gulp.dest('src/css')).pipe(bs.stream())
 }
 
 export const watch = () => {
